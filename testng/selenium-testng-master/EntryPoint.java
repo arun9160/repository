e05d66f209c0798981dd179914ab7ad4149cb69a
ArunKrishna <ArunKrishna@172.20.10.3>
2020-06-10 13:01:14 +0530

    The comits has been made

---
+++ testng/selenium-testng-master/EntryPoint.java
@@ -0,0 +1,16 @@
+package CBTTestNG.TestNG;
+
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+
+public class EntryPoint {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		TestListenerAdapter tla = new TestListenerAdapter();
+		TestNG testng = new TestNG();
+		testng.setTestClasses(new Class[] { TestNGBasic.class, TestNGLogin.class, TestNGTodo.class , TestNGDD.class});
+		
+		testng.addListener(tla);
+		testng.run();
+	}
+}
